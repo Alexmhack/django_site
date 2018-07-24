52cd661af420b91ec7f924cc78134f02c04508d1
alexHooper <alexmohack@gmail.com>
2018-07-25 00:52:49 +0530

    correct the url

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('accounts/login/', views.login, name='login'),
-    path('accounts/login/', views.logout, name='logout', kwargs={'next_page': '/'}),
+    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
