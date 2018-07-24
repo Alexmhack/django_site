a3748a527264b88c58c0224bf7506697018eef48
alexHooper <alexmohack@gmail.com>
2018-07-24 16:12:50 +0530

    correct error by adding () to timezone.now

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
 	published_date = models.DateTimeField(blank=True, null=True)
 
 	def publish(self):
-		self.published_date = timezone.now
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
