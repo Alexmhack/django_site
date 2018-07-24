b9ccbb6548775f2b23aa5833c30c3276800f5edf
alexHooper <alexmohack@gmail.com>
2018-07-25 00:08:31 +0530

    reverse the posts on post_list view

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .forms import PostForm
 
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')[::-1]
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
 
