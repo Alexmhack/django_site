1c548000880ea697ee0b9e09fd7d8d9edf077cea
alexHooper <alexmohack@gmail.com>
2018-07-24 11:48:28 +0530

    add url for post_draft_list view

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	path('post/<int:pk>/detail/', views.post_detail, name='post_detail'),
 	path('post/new/', views.post_new, name='post_new'),
 	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+	path('post/drafts/', views.post_draft_list, name='post_draft_list'),
 ]
