33c16826d5e512cab06247a0734c6e81a689c6b4
alexHooper <alexmohack@gmail.com>
2018-07-24 16:21:13 +0530

    add url for post_publish

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 	path('post/new/', views.post_new, name='post_new'),
 	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 	path('post/drafts/', views.post_draft_list, name='post_draft_list'),
+	path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 ]
