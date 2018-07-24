89c7432689f3a1048b1d976107fd0998f6e9c4d2
alexHooper <alexmohack@gmail.com>
2018-07-25 00:14:26 +0530

    add protection to our views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
 from .forms import PostForm
@@ -10,11 +11,13 @@ def post_list(request):
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+@login_required
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
 	if request.method == 'POST':
 		form = PostForm(request.POST)
@@ -29,6 +32,7 @@ def post_new(request):
 	return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == 'POST':
@@ -44,23 +48,27 @@ def post_edit(request, pk):
 	return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_draft_list(request):
 	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
 	return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
+@login_required
 def post_publish(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	post.publish()
 	return redirect('post_detail', pk=pk)
 
 
+@login_required
 def post_delete(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	post.delete()
 	return redirect('post_list')
 
 
+@login_required
 def post_delete_view(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_delete_view.html', {'post': post})
