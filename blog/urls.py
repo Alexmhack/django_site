8a601d30046e50ff13cee470fc1809ea8b745206
alexHooper <alexmohack@gmail.com>
2018-07-24 10:31:42 +0530

    add url for editing post

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 	path('', views.post_list, name='post_list'),
 	path('post/<int:pk>/detail/', views.post_detail, name='post_detail'),
+	path('post/new/', views.post_new, name='post_new'),
 ]
